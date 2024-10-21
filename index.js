b9401924087714218bd9da8ef80457c1234342d2
Akanksha Patil <84930402+akankshapatil2015@users.noreply.github.com>
2024-10-21 23:58:22 +0530

    log changes

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// Get the leads from the localStorage
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  // Save the myLeads array to localStorage
-  // PS: remember JSON.stringify()
-  localStorage.setItem("leadsFromLocalStorage", JSON.stringify(myLeads));
-  renderLeads();
-
-  // To verify that it works:
-  console.log(localStorage.getItem("leadsFromLocalStorage"));
-
-  // To verify that it works:
-  console.log(localStorage.getItem("leadsFromLocalStorage"));
+   myLeads.push(inputEl.value);
+   inputEl.value = "";
+   localStorage.setItem("myLeads", JSON.stringify(myLeads));
+   renderLeads();
 });
 
 function renderLeads() {
