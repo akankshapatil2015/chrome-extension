5e38b8535950a521a9a51f3e9331dfea102380dd
Akanksha Patil <84930402+akankshapatil2015@users.noreply.github.com>
2024-10-21 15:56:15 +0530

    Added addEventListenr

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
 
-function saveLead() {
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function () {
   console.log("Button clicked!");
-}
+});
