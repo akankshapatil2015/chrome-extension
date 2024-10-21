bfe9c60960165466930596407ae4e6eae108e632
Akanksha Patil <84930402+akankshapatil2015@users.noreply.github.com>
2024-10-21 17:01:21 +0530

    Added <a> tag

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  // 2. Call the renderLeads() function
   renderLeads();
 });
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
   }
   ulEl.innerHTML = listItems;
 }
