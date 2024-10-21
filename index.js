a940b8f0b53aed1c4d06c74a6175fce1d362af9f
Akanksha Patil <84930402+akankshapatil2015@users.noreply.github.com>
2024-10-21 16:36:47 +0530

    Different method

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-// 2. Grab the unordered list and store it in a const variable called ulEl
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
@@ -9,7 +8,12 @@ inputBtn.addEventListener("click", function () {
   console.log(myLeads);
 });
 
-// Replace .textContent with .innerHTML and use <li> tags
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
+let listItems = "";
 for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+  // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+  listItems += "<li>" + myLeads[i] + "</li>"
 }
+// 3. Render the listItems inside the unordered list using ulEl.innerHTML
+ulEl.innerHTML = listItems;
